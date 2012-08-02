200d806b807e5a4ad85f1db33914f7c4a55afdb6
Chris Price <chris@puppetlabs.com>
2012-08-02 12:38:47 -0700

    add project.clj and .gitignore

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject puppetdb "0.1"
+  :description "Scratch code related to puppetdb"
+  :dependencies [[org.clojure/clojure "1.4.0"]
+                 ]
+
+  :dev-dependencies [[criterium "0.2.1"]]
+
+;  :aot [com.puppetlabs.puppetdb.core]
+;  :main com.puppetlabs.puppetdb.core
+)
