afabc45d6678f4e16ab6f48b2a18367ba5a75734
Chris Price <chris@puppetlabs.com>
2013-03-03 12:07:44 -0800

    Basic producer functionality working and tested

--- src/clj/concurrent/prod_con.clj
+++ src/clj/concurrent/prod_con.clj
@@ -12,89 +12,98 @@
         (cons next-item (iterator-fn->lazy-seq f))))))
 
 (defn work-queue
-  [size]
-  (if (= 0 size)
-    (java.util.concurrent.LinkedBlockingQueue.)
-    (java.util.concurrent.ArrayBlockingQueue. size)))
+  ;; TODO docs
+  ([] (work-queue 0))
+  ([size]
+    (if (= 0 size)
+      (java.util.concurrent.LinkedBlockingQueue.)
+      (java.util.concurrent.ArrayBlockingQueue. size))))
 
-(defn work-queue-seq
-  ([queue]
-    (lazy-seq
-      (do
-        (println "initializing work queue seq")
-        (let [work (.take queue)]
-          (println "took some work off of the queue")
-          (if (nil? work)
-            (do
-              ;; TODO doc
-              (.put queue nil)
-              '())
-            (cons work (lazy-seq work-queue-seq queue))))))))
+(def work-complete-sentinel (Object.))
 
-(defn work-seq
-  ([work-fn]
-    (println "Attempting to get work seq for work-fn")
-    (work-seq work-fn (work-fn)))
-  ([work-fn next-work]
-    (println "Attempting to get work seq for work-fn with next-work:" next-work)
-    (if-not (nil? next-work)
-      (cons next-work (lazy-seq work-seq work-fn))
-      '())))
+(defn work-queue->seq
+  ;; TODO docs
+  [queue]
+  (iterator-fn->lazy-seq
+    (fn []
+      (let [next-item (.take queue)]
+        (if (= work-complete-sentinel next-item)
+          (do
+            ;; TODO: doc
+            (.put queue work-complete-sentinel)
+            nil)
+          next-item)))))
 
+;(defn work-seq
+;  ([work-fn]
+;    (println "Attempting to get work seq for work-fn")
+;    (work-seq work-fn (work-fn)))
+;  ([work-fn next-work]
+;    (println "Attempting to get work seq for work-fn with next-work:" next-work)
+;    (if-not (nil? next-work)
+;      (cons next-work (lazy-seq work-seq work-fn))
+;      '())))
+;
 (defn producer
-  [work-fn num-threads max-work]
-  (let [queue   (work-queue max-work)
-        workers (doall
-                  (for [i (range num-threads)]
-                    (do
-                      (println "Creating producer future" i)
-                      (future
-                        (doseq [work (work-seq work-fn)]
-                          (.put queue work))))))]
-    (println "Done creating producer workers")
-    {:queued-work (work-queue-seq queue)
-     :workers     workers}))
-
-(defn consumer
-  [producer work-fn num-threads max-results]
-  (let [result-queue (work-queue max-results)
-        workers      (for [_ (range num-threads)]
-                        (future (doseq [work (:queued-work producer)]
-                                  (.put result-queue (work-fn work)))))]
-    (work-queue-seq result-queue)))
-
-(def next-work-id (atom 1))
-
-(defn create-work
-  []
-  (println "create-work called")
-  (Thread/sleep 200)
-  (println "create-work: back from sleep")
-  (let [_    (println "current work id" @next-work-id)
-        work (swap! next-work-id inc)
-        _    (println "got next  work id" work)]
-    (println "create-work about to print thread info")
-    (println "current thread:" (Thread/currentThread))
-    (println "current thread id:" (.getId (Thread/currentThread)))
-    (println "Produced work"  work " on thread " (.getId (Thread/currentThread)))
-    work))
-
-(defn do-work
-  [work]
-  (println "Doing work:" work "  on thread " (.getId (Thread/currentThread))))
-
-
-(defn tryit
-  []
-  (let [prod    (producer create-work 5 20)
-        _       (println "Created producer")
-;        results (consumer prod do-work 10 20)
-        _       (println "Created consumer")]
-    (println "Main driver about to start trying to retrieve results")
-;    (doseq [result results]
-;      (println "Main driver: got result: " result))))
-    (doseq [worker (:workers prod)]
-      (println "Waiting for worker to finish")
-      (println "Worker finished:" @worker))
-  )
-  (println "Done."))
+  ([work-fn num-threads]
+    (producer work-fn num-threads 0))
+  ([work-fn num-threads max-work]
+    (let [queue    (work-queue max-work)
+          workers (doall
+                    (for [i (range num-threads)]
+                      (do
+                        (println "Creating producer future" i)
+                        (future
+                          (let [work-count (atom 0)]
+                            (doseq [work (iterator-fn->lazy-seq work-fn)]
+                              (.put queue work)
+                              (swap! work-count inc))
+                            (.put queue work-complete-sentinel)
+                            @work-count)))))]
+      (println "Done creating producer workers")
+      {:queued-work (work-queue->seq queue)
+       :workers     workers})))
+;
+;(defn consumer
+;  [producer work-fn num-threads max-results]
+;  (let [result-queue (work-queue max-results)
+;        workers      (for [_ (range num-threads)]
+;                        (future (doseq [work (:queued-work producer)]
+;                                  (.put result-queue (work-fn work)))))]
+;    (work-queue-seq result-queue)))
+;
+;(def next-work-id (atom 1))
+;
+;(defn create-work
+;  []
+;  (println "create-work called")
+;  (Thread/sleep 200)
+;  (println "create-work: back from sleep")
+;  (let [_    (println "current work id" @next-work-id)
+;        work (swap! next-work-id inc)
+;        _    (println "got next  work id" work)]
+;    (println "create-work about to print thread info")
+;    (println "current thread:" (Thread/currentThread))
+;    (println "current thread id:" (.getId (Thread/currentThread)))
+;    (println "Produced work"  work " on thread " (.getId (Thread/currentThread)))
+;    work))
+;
+;(defn do-work
+;  [work]
+;  (println "Doing work:" work "  on thread " (.getId (Thread/currentThread))))
+;
+;
+;(defn tryit
+;  []
+;  (let [prod    (producer create-work 5 20)
+;        _       (println "Created producer")
+;;        results (consumer prod do-work 10 20)
+;        _       (println "Created consumer")]
+;    (println "Main driver about to start trying to retrieve results")
+;;    (doseq [result results]
+;;      (println "Main driver: got result: " result))))
+;    (doseq [worker (:workers prod)]
+;      (println "Waiting for worker to finish")
+;      (println "Worker finished:" @worker))
+;  )
+;  (println "Done."))
--- test/clj/concurrent/test/prod_con.clj
+++ test/clj/concurrent/test/prod_con.clj
@@ -2,15 +2,25 @@
   (:use clojure.test
         concurrent.prod-con))
 
+(defn simple-work-stack
+  [size]
+  {:pre  [(pos? size)]}
+  (let [original-work   (range size)
+        remaining-work  (atom (range size))
+        counter         (atom 0)
+        iterator-fn     (fn []
+                          (swap! counter inc)
+                          (let [next-item (first @remaining-work)]
+                            (swap! remaining-work next)
+                            next-item))]
+    {:original-work   original-work
+     :remaining-work  remaining-work
+     :counter         counter
+     :iterator-fn     iterator-fn}))
+
 (deftest test-iterator-fn->lazy-seq
-  (let [stack       (atom (range 5))
-        counter     (atom 0)
-        iterator-fn (fn []
-                      (swap! counter inc)
-                      (let [next-item (first @stack)]
-                        (swap! stack next)
-                        next-item))
-        iterator-seq (iterator-fn->lazy-seq iterator-fn)]
+  (let [{:keys [counter iterator-fn]} (simple-work-stack 5)
+        iterator-seq                  (iterator-fn->lazy-seq iterator-fn)]
     (testing "iterator-fn is not called until seq is accessed"
       (is (= 0 @counter)))
     (testing "iterator-fn is called exactly once if we take one item from the seq"
@@ -25,3 +35,25 @@
     (testing "iterator-fn is called once for each item in the seq, plus once for the final nil if we walk the whole seq"
       (doseq [i iterator-seq] i)
       (is (= 6 @counter)))))
+
+(deftest test-work-queue->seq
+  (let [queue (work-queue)]
+    (doseq [i (range 5)]
+      (.put queue i))
+    (.put queue work-complete-sentinel)
+    (let [queue-seq  (work-queue->seq queue)]
+      (is (= (range 5) queue-seq)))))
+
+(deftest test-producer
+  (testing "with a single worker"
+    (let [{:keys [counter iterator-fn
+                  original-work]} (simple-work-stack 5)
+          p                       (producer iterator-fn 1)
+          {:keys [workers queued-work]} p]
+        (testing "number of workers matches what we requested"
+          (is (= 1 (count workers))))
+        (testing "worker completed the correct number of work items"
+          (let [worker (first workers)]
+            (is (= 5 @worker))))
+        (testing "work queue contains the correct work items"
+          (is (= original-work queued-work))))))
